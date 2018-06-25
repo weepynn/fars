2e321859ccad69d967d1fb8d310c78f18667c8f2
weepynn <39117485+weepynn@users.noreply.github.com>
2018-06-25 16:34:07 +0800

    Update test2.R

--- tests/testthat/test2.R
+++ tests/testthat/test2.R
@@ -1,3 +1,8 @@
-test_that("Check number of rows for 2013 accidents file", {
-  testthat::expect_equal(nrow(fars_read_years(2013)[[1]]), 30202)
+context("all")
+
+## TODO: Rename context
+## TODO: Add more tests
+
+test_that("make_filename works", {
+  expect_equal(make_filename(2013), paste0(system.file("extdata", package="fars"), "/accident_", 2013, ".csv.bz2"))
 })
