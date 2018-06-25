26307d150c963cd9666941e1d152db79b4119a92
weepynn <39117485+weepynn@users.noreply.github.com>
2018-06-25 17:01:07 +0800

    Update test2.R

--- tests/testthat/test2.R
+++ tests/testthat/test2.R
@@ -1,8 +1,24 @@
-context("all")
+context("Test the basic functionality of the package")
 
-## TODO: Rename context
-## TODO: Add more tests
+library(dplyr)
+library(maps)
 
-test_that("make_filename works", {
-  expect_equal(make_filename(2013), paste0(system.file("extdata", package="fars"), "/accident_", 2013, ".csv.bz2"))
+setwd(system.file("extdata", package = "fars"))
+
+test_that("fars_read() works correctly", {
+     expect_is(fars_read("accident_2015.csv.bz2"), "tbl_df")
+     expect_error(fars_read("accident_2016.csv.bz2"))
+})
+
+test_that("fars_summarize_years() works correctly", {
+     expect_is(fars_summarize_years(2013:2015), "tbl_df")
+     expect_is(fars_summarize_years(list(2013, 2014)), "tbl_df")
+     expect_equal(names(fars_summarize_years(2013:2015)), c("MONTH", 2013:2015))
+     expect_error(fars_summarize_years(2016))
+})
+
+test_that("fars_map_state() works correctly", {
+     expect_silent(fars_map_state(1, 2015))
+     expect_error(fars_map_state(100, 2015))
+     expect_error(fars_map_state(1, 2016))
 })
